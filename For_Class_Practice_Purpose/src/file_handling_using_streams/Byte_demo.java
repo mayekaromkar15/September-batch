70f67542e62d54fe7c67b1cc060400b7eafcecc2
omkar <mayekaromkar15@gmail.com>
2020-09-24 21:02:28 +0530

    commmited on 24 sep 2020 while started to practice

---
+++ For_Class_Practice_Purpose/src/file_handling_using_streams/Byte_demo.java
@@ -0,0 +1,44 @@
+package file_handling_using_streams;
+
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Byte_demo {
+
+	public static void main(String[] args) {
+
+		FileOutputStream fos = null;
+		BufferedOutputStream bos = null;
+		
+		try {
+			fos = new FileOutputStream("first_try.txt");
+			bos = new BufferedOutputStream(fos);
+			
+			
+			String message="\nI am new message tooooo";
+			byte b[]=message.getBytes();
+			bos.write(b);
+			System.out.println("Your message is written inside the file");
+			
+		} catch (FileNotFoundException  e) {
+			System.out.println(e);
+		} catch (IOException e) {
+			System.out.println(e);
+		}finally {
+			try {
+				bos.flush();
+				bos.close();
+				fos.close();
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+			
+		}
+		
+		
+		
+	}
+
+}
